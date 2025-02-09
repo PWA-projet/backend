6f18de563ece0f8669b1c39f8f42fe92178873ca
thibaut009 <gabet.thibaut@gmail.com>
2025-02-09 23:30:45 +0100

    Update

--- app/models/channel.ts
+++ app/models/channel.ts
@@ -36,7 +36,7 @@ export default class Channel extends BaseModel {
 
     // Boucle pour générer une clé unique
     do {
-      key = randomBytes(4).toString('hex').toUpperCase()  // Générer une clé aléatoire de 6 caractères
+      key = randomBytes(4).toString('hex').toUpperCase()  // Générer une clé aléatoire de 8 caractères
       exists = !!(await Channel.query().where('key', key).first())  // Vérifier si la clé existe déjà
     } while (exists)  // Si la clé existe, générer une nouvelle clé
 
--- database/migrations/1739006110645_create_channels_table.ts
+++ database/migrations/1739006110645_create_channels_table.ts
@@ -7,7 +7,7 @@ export default class extends BaseSchema {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('name').notNullable()
-      table.string('key').notNullable().unique()
+      table.string('key', 8).notNullable().unique()
 
       table.timestamp('created_at')
       table.timestamp('updated_at')
